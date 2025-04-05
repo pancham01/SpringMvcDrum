34bb6c8c6bf92292539051db9cf6812cfbda7f23
pancham01 <pancham9557@gmail.com>
2025-04-05 10:44:32 +0530

    update

--- src/main/java/com/drum/controller/NewController.java
+++ src/main/java/com/drum/controller/NewController.java
@@ -4,14 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/v1/")
-public class WelcomeController {
+@RequestMapping("/v2/")
+public class NewController {
 
 	@RequestMapping("/")
 	public String getResponse()
 	{
-		System.out.println("WelcomeController.getResponse()");
-		return "hello";
+		System.out.println("NewController.getResponse()");
+		return "welcome";
 	}
 	
 	
@@ -19,9 +19,7 @@ public class WelcomeController {
 	@RequestMapping("/login")
 	public String loginRequest()
 	{
-		System.out.println("loginRequest.getResponse()");
+		System.out.println("NewController.getResponse()");
 		return "profile";
 	}
 }
-
-
